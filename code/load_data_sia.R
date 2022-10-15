50b2856ebeeef91881bfaa9af50901edbac2c3e3
Filipe Cavalcanti <f.matheus.cavalcanti@gmail.com>
2022-10-15 00:14:38 -0300

    Update load_data_sia.R

--- code/load_data_sia.R
+++ code/load_data_sia.R
@@ -2,7 +2,7 @@ library(bigrquery)
 
 bq_auth(path = 'gcp-auth-dev.json')
 
-bq_dataset_create("opensus-dev.sia")
+#bq_dataset_create("opensus-dev.sia")
 
 bq_perform_query("
   LOAD DATA OVERWRITE sia.pa_sergipe
@@ -10,5 +10,3 @@ bq_perform_query("
   format = 'parquet',
   uris = ['gs://opensus-dev/br_ms_sia/pase/data/*parquet']);",
   "opensus-dev")
-
-
